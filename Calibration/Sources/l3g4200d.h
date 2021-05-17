ee636ea66761b0c399087008e595bd8bd2600c1f
Zach-commits <79881907+Zach-commits@users.noreply.github.com>
2021-05-17 21:53:24 +1000

    Add files via upload

---
+++ Calibration/Sources/l3g4200d.h
@@ -0,0 +1,60 @@
+#ifndef L3G4200D_HEADER
+#define L3G4200D_HEADER
+
+// file l3g4200d.h
+//   public interface for the l3g4200d sensor suite
+//   this interface allows you to initalise the sensors
+//   and read the raw data values
+//   
+//   these values require further scaling according to the 
+//   datasheets
+
+#include "iic.h"
+#include "accelerometer.h"
+
+// data structures containing the raw values
+
+typedef struct GyroRaw {
+  int x;
+  int y;
+  int z;
+} GyroRaw;
+
+typedef struct GyroOffsets {
+  float x;
+  float y;
+  float z;
+} GyroOffsets;
+
+
+
+
+typedef struct MagRaw {
+  int x;
+  int y;
+  int z;
+} MagRaw;
+
+
+// Initialise each sensor
+IIC_ERRORS iicSensorInit();
+
+// Get the raw acceleration data from the sensor
+IIC_ERRORS getRawDataAccel(AccelRaw *raw_data);
+
+
+// Get the raw magnetic data from the sensor
+IIC_ERRORS getRawDataMagnet(MagRaw *raw_data);
+
+
+// Get the raw gyro data from the sensor
+IIC_ERRORS getRawDataGyro(GyroRaw *raw_data);
+
+// Sets the offsets
+void calibrate_gryo(GyroRaw *raw_data, GyroOffsets *scaled_data);
+
+
+// Correct info, after calibratiio
+void cleaned_gryo(GyroRaw *raw_data, GyroOffsets *scaled_data);
+
+#endif
