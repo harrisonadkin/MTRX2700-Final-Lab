e0516d47abebcf9f63300545407e7af33fefb39f
harrisonadkin <59522360+harrisonadkin@users.noreply.github.com>
2021-05-25 13:30:51 +1000

    Calibration??

---
+++ Demonstration/Sources/calibrate.h
@@ -0,0 +1,12 @@
+#ifndef CALIBRATE_HEADER
+#define CALIBRATE_HEADER
+
+
+//#pragma CODE_SEG __NEAR_SEG NON_BANKED 
+//__interrupt void ButtonISR(void);  
+
+void calibrate(void);
+void servoInit(void);
+
+
+#endif
